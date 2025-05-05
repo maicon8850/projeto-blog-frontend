7deb6122bc10adcd10cdf3a73b5bb87455743102
maicon-douglas <maicon.douglas3527@gmail.com>
2025-05-04 21:53:31 -0300

    chore(config): ajustes nos arquivos de ambiente e proxy para facilitar o desenvolvimento

---
+++ src/environments/environment.development.ts
@@ -0,0 +1,13 @@
+export const environment = {
+  production: false,
+  id: 0,
+  nome: '',
+  usuario: '',
+  senha: '',
+  foto: '',
+  token: '',
+  //api: 'https://blogpessoal-nest-qz4t.onrender.com',
+  api: '/api',
+
+};
+//////// PARA DESENVOLVIMENTO QUANDO EU SUBIR NO AR ////////
---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  id: 0,
+  nome: '',
+  usuario: '',
+  senha: '',
+  foto: '',
+  token: '',
+  api: '/api',   // para o spring 
+
+};
